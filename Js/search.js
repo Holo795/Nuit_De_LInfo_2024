9f735f4a44066a8bc3096df3c6717fea81611e10
simon <simon.metivier@etudiant.univ-lr.fr>
2024-12-06 02:00:27 +0100

    `[Clean]`: Le code est nettoyer + pas de clé google

--- Js/search.js
+++ Js/search.js
@@ -11,11 +11,12 @@ document.addEventListener("DOMContentLoaded", function () {
         const resultsContainer = document.getElementById("resultsContainer");
         if (!resultsContainer) return;
 
-        const apiKeyGoogle = 'AIzaSyB-G1hAg7EbZVtSsxrjL0Sjm2cER69QUOk';
-        const cx = '20a985c2483854fa3';
+        const apiKeyGoogle = '';
+        const cx = '';
 
         // Traduire les emojis en slugs
         const querySlugs = await convertEmojisToSlugs(query);
+        console.log(querySlugs);
 
         // Créer l'URL de la recherche Google Custom Search
         const url = `https://www.googleapis.com/customsearch/v1?q=${encodeURIComponent(querySlugs)}&key=${apiKeyGoogle}&cx=${cx}`;
@@ -44,22 +45,33 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Fonction pour convertir les emojis en slugs
     async function convertEmojisToSlugs(query) {
-        const apiKeyEmoji = "aaab5aea8be43727a7055a906cfbe1add35c6a47";
+        const apiKeyEmoji = "";
         const apiUrl = `https://emoji-api.com/emojis?access_key=${apiKeyEmoji}`;
 
         const response = await fetch(apiUrl);
-
         const emojisData = await response.json();
         const emojiMap = new Map(
             emojisData.map((emoji) => [emoji.character, emoji.slug]) // Associer chaque emoji à son slug
         );
 
-        // Extraire chaque caractère de la chaîne et récupérer le slug si c'est un emoji
-        const slugs = Array.from(query)
-            .map((char) => emojiMap.get(char) || char) // Remplace les emojis par leurs slugs
-            .filter((slug) => typeof slug === "string"); // Filtre pour éliminer les caractères non reconnus
+        // Extraire chaque caractère de la chaîne et récupérer le slug, puis traduire si nécessaire
+        const slugs = await Promise.all(
+            Array.from(query)
+                .map(async (char) => {
+                    const slug = emojiMap.get(char);
+                    if (slug) {
+                        return extractImportantWord(slug); // Extraire et traduire
+                    }
+                    return char; // Si ce n'est pas un emoji, on garde le caractère
+                })
+        );
 
-        return slugs.join(" "); // Retourner une chaîne de slugs séparés par des espaces
+        return slugs.join(" "); // Retourner une chaîne de slugs traduits séparés par des espaces
     }
 
+    // Fonction pour extraire la partie importante d'un slug
+    function extractImportantWord(slug) {
+        const parts = slug.split('-'); // Divise le slug par les tirets
+        return parts.slice(2).join('-'); // Enlève les deux premiers éléments et retourne le reste
+    }
 });
